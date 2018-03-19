3a4c873f4817e433acc4cc3f44a3eebb72b578bc
jerrrrycho <jurek.pakula@gmail.com>
2018-03-19 16:45:12 +0100

    Addnibg manhattan metric

--- src/main/java/ksr/metric/ChebyshevMetric.java
+++ src/main/java/ksr/metric/ChebyshevMetric.java
@@ -1,4 +1,26 @@
 package ksr.metric;
 
-public class ChebyshevMetric {
+import ksr.knn.Entry;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ChebyshevMetric implements Metric {
+
+    @Override
+    public double dist(Entry a, Entry b) {
+        Set<Integer> indexes = new HashSet<>(a.getWordsMap().keySet());
+        indexes.addAll(b.getWordsMap().keySet());
+        double distance = 0;
+
+        for (int index : indexes) {
+            Integer val1 = a.getWordsMap().get(index);
+            Integer val2 = b.getWordsMap().get(index);
+            if (val1 == null) val1 = 0;
+            if (val2 == null) val2 = 0;
+            distance = Math.max(distance, Math.abs(val1-val2));
+        }
+
+        return distance;
+    }
 }
